05b3f626ba37a221c27330faf8b15a00f543cff9
utkarsh srivastava <srivastava.u002@gmail.com>
2023-06-09 12:16:20 +0530

    api changes

--- routes/user.js
+++ routes/user.js
@@ -16,9 +16,10 @@ router.get('/googlelogin',passport.authenticate('google', {
 // })
 
 
-router.get('/login',passport.authenticate('google',{
-    successRedirect:process.env.FRONTEND_URL
-})
+router.get('/login',(req,res,next)=>{
+    res.send('logged in')
+
+}
 )
 
 
